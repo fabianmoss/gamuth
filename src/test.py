089a201df4355d318387208e5717a5987bfcae6f
Fabian C. Moss <fabianmoss@gmail.com>
2020-04-19 16:59:00 +0200

    get labels from Euler coordinates

--- src/test.py
+++ src/test.py
@@ -1,11 +1,19 @@
 from theory import Tone, Interval
+import numpy as np
 
 if __name__ == "__main__":
-    s = Tone(2,3,1)
-    t = Tone(-2,1,4)
+    s = Tone(0,-1,2) # Eb''0
+    t = Tone(0,-1,0) # G0
     i = Interval(source=s,target=t)
+
+    print(s.label)
+    print(t.label)
+
+    # print(s.accidentals)
+    # print(s.step)
     
-    print(s.euler_coordinate)
-    print(t.euler_coordinate)
-    print(i.source.euler_coordinate)
-    print(i.euclidean_distance())
+    # print(s.euler_coordinate)
+    # print(t.euler_coordinate)
+    # print(i.source.euler_coordinate)
+    # print(i.interval)
+    # print(i.euclidean_distance())
