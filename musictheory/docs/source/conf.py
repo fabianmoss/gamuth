1d879a77688524fc4ca1a0c7a9d9dcc667d252d2
Fabian C. Moss <fabianmoss@gmail.com>
2020-05-17 15:40:22 +0200

    change theme

--- musictheory/docs/source/conf.py
+++ musictheory/docs/source/conf.py
@@ -68,7 +68,7 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = "kr"#"haiku"#'alabaster', "kr"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
@@ -78,4 +78,4 @@ html_static_path = ['_static']
 # link to master doc
 master_doc = 'index'
 
-html_logo = './_static/logo2.png'
+# html_logo = './_static/logo2.png'
